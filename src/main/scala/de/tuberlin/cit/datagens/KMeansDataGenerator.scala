cfa9d5b7f297b4700ebdc0333219ff471f3589d2
Houkun Zhu <diskun.zhu@gmail.com>
2021-05-04 11:49:16 +0800

    generate kmeans data to hdfs

--- src/main/scala/de/tuberlin/cit/datagens/KMeansDataGenerator.scala
+++ src/main/scala/de/tuberlin/cit/datagens/KMeansDataGenerator.scala
@@ -1,23 +1,24 @@
 package de.tuberlin.cit.datagens
 
-import java.io.PrintWriter
 
 import breeze.linalg.DenseVector
 import breeze.stats.distributions.Rand
-
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 case class MeanConf(mean: DenseVector[Double], stdDev: Double)
 
 object KMeansDataGenerator {
 
   def main(args: Array[String]): Unit = {
     if (args.length < 3) {
-      println("KMeansDataGenerator <samples> <cluster> <output>")
+      println("KMeansDataGenerator <samples> <cluster> <output> <defaultHdfsFs")
       System.exit(1)
     }
 
     val n = args(0).toInt
     val k = args(1).toInt
     val outputPath = args(2)
+    val defaultFs = args(3)
 
     val dim = 2
     val stdDev = .012
@@ -27,14 +28,19 @@ object KMeansDataGenerator {
     val centers = uniformRandomCenters(dim, k, stdDev)
     val centerDistribution = Rand.choose(centers)
 
-    val writer = new PrintWriter(outputPath)
+    System.setProperty("HADOOP_USER_NAME", "drms")
+    val path = new Path(outputPath)
+    val conf = new Configuration()
+    conf.set("fs.defaultFS", defaultFs)
+    val fs = FileSystem.get(conf)
+    val os = fs.create(path)
     (1 to n).foreach(_ => {
       val MeanConf(mean, stdDev) = centerDistribution.draw()
       val p = mean + DenseVector.rand[Double](mean.length, Rand.gaussian(0, stdDev))
-      writer.println(p.toArray.mkString(" "))
+      os.writeBytes(p.toArray.mkString(" ")+"\n")
     })
-    writer.flush()
-    writer.close()
+    fs.close()
+
   }
 
   def uniformRandomCenters(dim: Int, k: Int, stdDev: Double): Seq[MeanConf] = {
