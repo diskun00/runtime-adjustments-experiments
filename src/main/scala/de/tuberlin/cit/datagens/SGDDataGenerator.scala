84786d0cd041efd69deeac56d9846fb346d94431
Houkun Zhu <diskun.zhu@gmail.com>
2021-05-25 15:14:53 +0800

    add hdfs output to SGDDataGenerator

--- src/main/scala/de/tuberlin/cit/datagens/SGDDataGenerator.scala
+++ src/main/scala/de/tuberlin/cit/datagens/SGDDataGenerator.scala
@@ -1,22 +1,32 @@
 package de.tuberlin.cit.datagens
 
-import java.io.PrintWriter
-import java.util.concurrent.ThreadLocalRandom
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 
+import java.util.concurrent.ThreadLocalRandom
 import scala.math.pow
 
 object SGDDataGenerator {
   def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
-      Console.err.println("Usage: SGDDataGenerator <samples> <dimension> <output>")
+    if (args.length != 4) {
+      Console.err.println("Usage: SGDDataGenerator <samples> <dimension> <output> <defaultHdfsFs>")
       System.exit(-1)
     }
 
     val m = args(0).toInt
     val n = args(1).toInt
     val outputPath = args(2)
+    val defaultFs = args(3)
 
-    val writer = new PrintWriter(outputPath)
+
+    System.setProperty("HADOOP_USER_NAME", "drms")
+    val path = new Path(outputPath)
+    val conf = new Configuration()
+    conf.set("fs.defaultFS", defaultFs)
+    conf.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName);
+    conf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName);
+    val fs = FileSystem.get(conf)
+    val os = fs.create(path)
 
     for (_ <- 0 until m) {
       val x = ThreadLocalRandom.current().nextDouble() * 10
@@ -29,11 +39,10 @@ object SGDDataGenerator {
       val vector = polyvander(x, n - 1)
 
       // write to file in MLUtils format (label, feature1 feature2 ... featureN)
-      writer.println(label + "," + vector.mkString(" "))
+      os.writeBytes(label + "," + vector.mkString(" ") + "\n")
     }
 
-    writer.flush()
-    writer.close()
+    fs.close()
   }
 
   def polyvander(x: Double, order: Int): Array[Double] = {
