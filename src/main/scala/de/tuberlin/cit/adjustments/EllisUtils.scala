3ce370e6c83e5bf3197ef484a0ff64b66402bca4
mcd01 <57414190+mcd01@users.noreply.github.com>
2021-06-21 13:56:06 +0200

    create tables if they do not exist

--- src/main/scala/de/tuberlin/cit/adjustments/EllisUtils.scala
+++ src/main/scala/de/tuberlin/cit/adjustments/EllisUtils.scala
@@ -155,11 +155,37 @@ class EllisApplication() {
     ConnectionPool.singleton(s"jdbc:h2:$dbPath", "sa", "")
   }
 
+  def createTables(): Unit = {
+    DB localTx { implicit session =>
+      sql"""
+      CREATE TABLE IF NOT EXISTS app_event (
+      id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+      app_id VARCHAR(64) NOT NULL,
+      started_at TIMESTAMP NOT NULL,
+      finished_at TIMESTAMP,
+      UNIQUE (app_id, started_at));
+      """.execute()
+
+      sql"""
+      CREATE TABLE IF NOT EXISTS job_event (
+      app_event_id INT NOT NULL,
+      job_id INT NOT NULL,
+      started_at TIMESTAMP NOT NULL,
+      finished_at TIMESTAMP NOT NULL,
+      duration_ms INT NOT NULL,
+      scale_out INT NOT NULL,
+      PRIMARY KEY (app_event_id, job_id),
+      FOREIGN KEY (app_event_id) REFERENCES app_event (id));
+      """.execute()
+    }
+  }
+
   def computeInitialScaleOut(dbPath: String,
                              appSignature: String, minExecutors: Int, maxExecutors: Int, targetRuntimeMs: Int): Int = {
     logger.info(s"New request: ${dbPath}, ${appSignature}, ${minExecutors}, ${maxExecutors}, ${targetRuntimeMs}")
     if(dbPathApp != dbPath){
       init(dbPath)
+      createTables()
     }
 
     EllisUtils.computeInitialScaleOut(appSignature, minExecutors, maxExecutors, targetRuntimeMs)
