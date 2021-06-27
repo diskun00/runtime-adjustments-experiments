b21e0592d84201af92a9c895588e23e29c5647de
mcd01 <57414190+mcd01@users.noreply.github.com>
2021-06-27 23:16:17 +0200

    only transmit app end_time

--- src/main/scala/de/tuberlin/cit/adjustments/EnelScaleOutListener.scala
+++ src/main/scala/de/tuberlin/cit/adjustments/EnelScaleOutListener.scala
@@ -204,14 +204,10 @@ class EnelScaleOutListener(sparkContext: SparkContext, sparkConf: SparkConf) ext
 
     logger.info(s"Application ${applicationId} finished.")
 
-    val mapKey: String = f"appId=${applicationId}-jobId=${jobId}"
-
     val updateMap: Map[String, Any] = Map(
-      "end_time" -> applicationEnd.time,
-      "end_scale_out" -> infoMap(mapKey)("end_scale_out").toString.toInt
+      "end_time" -> applicationEnd.time
     )
     updateInformation(Option(applicationId), updateMap, "APPLICATION_END")
-
   }
 
   override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
